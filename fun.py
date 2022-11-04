becc93d7922ec08742b08763eb3caf0a4cbb8a4d
isashrestha <108517892+isashrestha@users.noreply.github.com>
2022-11-04 11:34:29 +0545

    3rd nov task

--- fun.py
+++ fun.py
@@ -1,11 +1,12 @@
 def vowel_count(word):
     vowels = 0
+    
     for i in word:
         if(i == "a" or i =="e" or i == "i" or i == "o" or i == "u"):
             vowels = vowels + 1;
     return vowels
 
-word = "watermelon"
+word = input("enter a word= ")
 print(vowel_count(word))
 
 
